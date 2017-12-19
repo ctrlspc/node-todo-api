8cd83edabd299ab82f4925d130a25643b569e42f
Jason <ctrlspc@gmail.com>
2017-12-19 20:46:52 +0000

    added a new route for getting a todo by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -8,7 +9,7 @@ var {User} = require('./models/user');
 var app = express();
 app.use(bodyParser.json());
 
-//POST /todos *****************
+// POST /todos *****************
 app.post('/todos', (req,res) => {
   var todo = new Todo({
     text:req.body.text
@@ -21,7 +22,7 @@ app.post('/todos', (req,res) => {
   });
 });
 
-//GET /todos **********************
+// GET /todos **********************
 app.get('/todos',(req,res) => {
   Todo.find().then((todos) => {
     res.send({todos});
@@ -30,6 +31,27 @@ app.get('/todos',(req,res) => {
   });
 });
 
+
+// GET /todos/:id ******************
+app.get('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(todo) {
+      return res.send({todo});
+    }
+
+    res.status(404).send();
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+})
 
 
 //*******************
